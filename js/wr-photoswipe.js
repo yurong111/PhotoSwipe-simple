f1d4fdcfb128128062a8c03d04c27307340a812e
hwr <1092021277@qq.com>
2017-11-14 17:45:22 +0800

    [fix] 优化

--- js/wr-photoswipe.js
+++ js/wr-photoswipe.js
@@ -39,45 +39,6 @@ Photoswipe = {
                 <div id="bar" class="bar"></div> \
             </div> \
         </div>';
-        // var html = '<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"> ' +
-        //     '<div class="pswp__bg"></div> ' +
-        //     '<div class="pswp__scroll-wrap"> ' +
-        //     '<div class="pswp__container"> ' +
-        //     '<div class="pswp__item"></div> ' +
-        //     '<div class="pswp__item"></div> ' +
-        //     '<div class="pswp__item"></div> ' +
-        //     '</div> ' +
-        //     '<div class="pswp__ui pswp__ui--hidden"> ' +
-        //     '<div class="pswp__top-bar"> ' +
-        //     '<div class="pswp__counter"></div> ' +
-        //     '<button class="pswp__button pswp__button--close" title="Close (Esc)"></button> ' +
-        //     '<button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>' +
-        //     '<div class="pswp__preloader">' +
-        //     '<div class="pswp__preloader__icn">' +
-        //     '<div class="pswp__preloader__cut">' +
-        //     '<div class="pswp__preloader__donut"></div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '<div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">' +
-        //     '<div class="pswp__share-tooltip"></div>' +
-        //     '</div>' +
-        //     '<button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">' +
-        //     '</button>' +
-        //     '<button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">' +
-        //     '</button>' +
-        //     '<div class="pswp__caption">' +
-        //     '<div class="pswp__caption__center"></div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '</div>' +
-        //     '<div id="progress-bar"> ' +
-        //     '<div class="progress"> ' +
-        //     '<div id="bar" class="bar"></div> ' +
-        //     '</div> ' +
-        //     '</div>';
 
         $('body').append(html);
     },
@@ -94,7 +55,7 @@ Photoswipe = {
                 setTimeout(function() {
                     Photoswipe._initPhotoSwipe(items);
                     document.getElementById('progress-bar').style.display = 'none'; //隐藏
-                }, 500)
+                }, 200)
                 return;
             }
 
